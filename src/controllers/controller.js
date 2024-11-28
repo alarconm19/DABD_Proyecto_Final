a020ac56bacccae70b0ffa39e4c63e2f059555e7
Pibe19 <marcosalarcon213@gmail.com>
2024-11-28 18:54:02 -0300

    update 1.4

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,11 +1,21 @@
 //controllers
 
 function getInscripcionEquipos(req, res) {
-    res.render('inscripcion-equipos');
+    res.render('inscripcion-equipos', { title: 'Inscripción de Equipos' });
+}
+
+function getCreacionTorneos(req, res) {
+    res.render('creacion-torneos', { title: 'Creacion de Torneos' });
+}
+
+function getRegistroEquipos(req, res) {
+    res.render('registro-equipos', { title: 'Registro de Equipos' });
 }
 
 
 module.exports = {
     // functions
     getInscripcionEquipos,
+    getCreacionTorneos,
+    getRegistroEquipos,
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,5 +10,8 @@ router.get('/', (req, res) => {
 // ejemplo
 // router.get('/about', controller.getAbout);
 router.get('/inscripcion-equipos', controller.getInscripcionEquipos);
+// router.get('/inscripcion-jugadores', controller.getInscripcionJugadores);
+router.get('/creacion-torneos', controller.getCreacionTorneos);
+router.get('/registro-equipos', controller.getRegistroEquipos);
 
 module.exports = router;
