270de0c3540d0f4acb6727c206da0951863ca14a
Pibe19 <marcosalarcon213@gmail.com>
2024-11-29 15:59:35 -0300

    update 2.3

--- Scripts/AgregarDivisionesCategorias.sql
+++ Scripts/AgregarDivisionesCategorias.sql
@@ -1,25 +1,25 @@
 --Creacion de Divisiones:
-INSERT INTO AFDB.dbo.Division
+INSERT INTO DABD.dbo.Division
 (Nombre)
 VALUES('A');
 
-INSERT INTO AFDB.dbo.Division
+INSERT INTO DABD.dbo.Division
 (Nombre)
 VALUES('B');
 
-INSERT INTO AFDB.dbo.Division
+INSERT INTO DABD.dbo.Division
 (Nombre)
 VALUES('C');
 
 --Creacion de Categorías
-INSERT INTO AFDB.dbo.Categoria
+INSERT INTO DABD.dbo.Categoria
 (Nombre, EdadMaxima, EdadMinima)
 VALUES('Maxi', 41, 45);
 
-INSERT INTO AFDB.dbo.Categoria
+INSERT INTO DABD.dbo.Categoria
 (Nombre, EdadMaxima, EdadMinima)
 VALUES('Súper', 46, 50);
 
-INSERT INTO AFDB.dbo.Categoria
+INSERT INTO DABD.dbo.Categoria
 (Nombre, EdadMaxima, EdadMinima)
 VALUES('Master', 51, 55);
